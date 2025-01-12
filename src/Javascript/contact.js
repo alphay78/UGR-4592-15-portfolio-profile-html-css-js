66ada1e2371fb6887c9bb983c4fbf4847497c274
Alpha <148535861+alphay78@users.noreply.github.com>
2025-01-12 13:48:26 +0300

    final commit

---
+++ src/Javascript/contact.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".navlist");
+
+hamburger.addEventListener("click", () => {
+  navList.style.display = navList.style.display === "flex" ? "none" : "flex";
+});
